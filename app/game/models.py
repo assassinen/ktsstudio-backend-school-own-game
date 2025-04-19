1edccda9565ce37b8638ba0a4a803e2c758863dd
inovikov <inovikov@astralinux.ru>
2025-04-19 10:05:12 +0300

    ruff

--- app/game/models.py
+++ app/game/models.py
@@ -1,22 +1,12 @@
-import sqlalchemy as sa
-from uuid import UUID
 from datetime import datetime
+from uuid import UUID
+
+import sqlalchemy as sa
+from sqlalchemy.orm import Mapped, mapped_column
 
 from app.game.statuses import GameStatus
 from app.store.database.sqlalchemy_base import db
-from sqlalchemy.orm import Mapped
-from sqlalchemy.orm import mapped_column
 
-
-# class Game(db):
-#     __tablename__ = "game"
-#
-#     uuid = Column(String, primary_key=True)
-#     chat_id = Column(BIGINT, nullable=False)
-#     status = Column(Enum(GameStatus), nullable=False)
-#     create_date = Column(DateTime, nullable=False)
-#     update_date = Column(DateTime, nullable=False)
-#
 
 class Game(db):
     __tablename__ = "game"
